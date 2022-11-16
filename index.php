211c4b17e8cea67ff565355ed2cc238d589eed4c
Muhammad Firdaus Al Rasyid <firdausalrasyid23@gmail.com>
2022-11-16 19:32:56 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa repellat excepturi voluptatem libero labore ratione asperiores laborum, amet fuga sapiente quaerat aperiam adipisci dignissimos consequuntur et at est neque. Magni?</p>
 </body>
 </html>
